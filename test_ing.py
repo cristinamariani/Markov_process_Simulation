94e4d402e4098d4c9d8b30206d65a50073374a47
Cristina Mariani D'Altri <cristina.mariani3@studio.unibo.it>
2022-01-19 15:23:24 +0100

    updated testing func in testing file

--- test_ing.py
+++ test_ing.py
@@ -3,16 +3,21 @@ import Functions
 import pytest
 
 
-def test_initialstate():
+#"initialstate" function testing
+
+# test 1: tests that the output is the expected value
+def test_initialstate_1():
     IS = np.array([0, 0, 1, 0, 0, 0, 0, 0])
     assert np.array_equal(Functions.initialstate('C'),IS)
+
+# test 2: tests that ValueError is raised when the input is not chosen among {A, B, C, D, E, F, G, H}    
+def test_initialstate_2():     
+    with pytest.raises(ValueError):
+        Functions.initialstate('abc')
+        Functions.initialstate(123) 
+            
     
-    
-    
-    
-    
-    
-    
+   
     
 
 
