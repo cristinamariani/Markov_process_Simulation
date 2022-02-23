026207aac325426a4f29a96a4bfb674004ce4899
Cristina Mariani D'Altri <cristina.mariani3@studio.unibo.it>
2022-02-23 17:45:45 +0100

    Added docstrings to functions in testing file

--- test_ing.py
+++ test_ing.py
@@ -18,7 +18,7 @@ T = np.matrix([[ 0.2,  0.3, 0.05, 0.06,  0.1, 0.04, 0.05,  0.2],
                [0.25, 0.05,  0.4, 0.07,  0.1, 0.03,    0,  0.1]])
 
 #Values for number of steps = 30 with initial state 'A' and random seed = 3
-IS = np.array([1, 0, 0, 0, 0, 0, 0, 0])
+IS = np.array([1, 0, 0, 0, 0, 0, 0, 0]) #initial state array
 number_steps = 30
 statdistr = np.array([0.12066125, 0.28382706, 0.18280321, 0.0398397, 0.13176984, 0.06221281, 0.0641203, 0.11476584])
 states = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
@@ -26,56 +26,57 @@ randomwalk = ['A', 'B', 'C', 'B', 'E', 'E', 'C', 'A', 'H', 'B', 'B', 'H', 'C', '
 fake_randomwalk1 = ['B', 'D', 'G', 'G', 'G', 'A', 'G', 'E', 'C']
 fake_randomwalk2 = ['A', 'F', 'G', 'F', 'G', 'A', 'H', 'G', 'B']
 
+
 #"initialstate" function testing
 
-# test 1: tests that the output is the expected value
 def test_initialstate_1():
+    """Test 1: tests that the output is the expected value"""
     assert np.array_equal(Functions.initialstate('A'), IS)    
-
-# test 2: tests that ValueError is raised when the input is not chosen among {A, B, C, D, E, F, G, H}    
-def test_initialstate_2():     
+    
+def test_initialstate_2(): 
+    """Test 2: tests that ValueError is raised when the input is not chosen among {A, B, C, D, E, F, G, H}"""    
     with pytest.raises(ValueError):
         Functions.initialstate('abc')
         Functions.initialstate(123)  
 
-# test 3: tests that the output array has 8 elements
 def test_initialstate_3():
+    """Test 3: tests that the output array has 8 elements"""
     assert len(Functions.initialstate('A')) == 8
-    
-# test 4: tests that, for different input, the output is different  
+     
 def test_initialstate_4():
+    """Test 4: tests that, for different input, the output is different""" 
     assert not np.array_equal(Functions.initialstate('E'),Functions.initialstate('C'))
     
-# test 5: tests that the output array has 1 element equal to 1 and the others equal to 0
 def test_initialstate_5():
+    """Test 5: tests that the output array has 1 element equal to 1 and the others equal to 0"""
     assert np.count_nonzero(Functions.initialstate('A')) == 1 #only one element is non-zero
     assert np.count_nonzero(Functions.initialstate('A') == 1) == 1 #the only one non-zero element is 1
             
  
 #"stat_distr" function testing 
 
-# test 1: tests that the calculated stationary distribution is the expected value
 def test_prob_distr_1():
+    """Test 1: tests that the calculated stationary distribution is the expected value"""
     func_outcome = Functions.prob_distr(T, IS, number_steps)[30] 
     assert np.allclose(func_outcome, statdistr)
 
-# test 2: tests that the sum of the elements of the stationary distribution array is equal to 1
 def test_prob_distr_2():
+    """Test 2: tests that the sum of the elements of the stationary distribution array is equal to 1"""
     assert math.isclose(np.sum(Functions.prob_distr(T, IS, number_steps)[30]),1)
-
-# test 3: tests that for different initial state, the stationary distribution is the same  
+ 
 def test_prob_distr_3():
+    """Test 3: tests that for different initial state, the stationary distribution is the same"""
     assert np.array_equal(Functions.prob_distr(T, Functions.initialstate('F'), 100)[100], Functions.prob_distr(T, Functions.initialstate('G'), 100)[100])
 
 
 #"rnd_walk" function testing
 
-# test 1: tests that the output is the expected value
 def test_rnd_walk_1():
+    """Test 1: tests that the output is the expected value"""
     assert Functions.rnd_walk(number_steps, T, 'A', states, 3) == randomwalk
     
-# test 2: tests that the probability distribution of the occurrences of the chain is approximately equal to the stationary distribution
 def test_rnd_walk_2():
+    """Test 2: tests that the probability distribution of the occurrences of the chain is approximately equal to the stationary distribution"""
     occurrences1 = collections.Counter(Functions.rnd_walk(1001000, T, 'A', states, 7)[-1000000:])
     probdistr1 = []
     probdistr1.append(round(occurrences1['A']/1000000, 2))
@@ -106,8 +107,8 @@ def test_rnd_walk_2():
 
 #"check_rnd_walk" function testing
 
-# test 1: tests that the given random walk does not contain forbidden transitions   
 def test_check_rnd_walk1():
+    """Test 1: tests that the given random walk does not contain forbidden transitions"""
     assert Functions.check_rnd_walk(Functions.rnd_walk(number_steps, T, 'B', states, 4))   
     assert Functions.check_rnd_walk(Functions.rnd_walk(number_steps, T, 'A', states, 1))
     assert not Functions.check_rnd_walk(fake_randomwalk1)
